d706275a292b8b4020c869744bf5319036916b72
hj-rxl <rubyxluna@gmail.com>
2025-01-02 16:33:48 +0900

    Revert "[버그수정] #1"
    This reverts commit 7222da3c42a372184c0cffafc8ab78bc351d1ddb.

--- maven-practice/src/main/java/kr/co/greenart/server/ServerStatusServlet.java
+++ maven-practice/src/main/java/kr/co/greenart/server/ServerStatusServlet.java
@@ -9,7 +9,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet({"/server/status"})
+@WebServlet({"/", "/index", "/server/status"})
 public class ServerStatusServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
